87eb25e3e669d00ae03488455869009b01997125
Pichborith Kong <pichborith@gmail.com>
2023-02-02 19:05:49 -0800

    update function to store tasks in localstorage and create function delete tas

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Task = ({ task, index }) => {
+const Task = ({ task, index, onClickDelete }) => {
   return (
     <li>
       <label htmlFor={`task-${index}`}>{task.task}</label>
       <div>
         <input type='checkbox' id={`task-${index}`} />
-        <button>Delete</button>
+        <button data-index={index} onClick={onClickDelete}>
+          Delete
+        </button>
       </div>
     </li>
   );
