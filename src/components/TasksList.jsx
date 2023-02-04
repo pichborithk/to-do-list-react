c26165cb7b2aa8efd2500522215d6b54cdcf2f4b
Pichborith Kong <pichborith@gmail.com>
2023-02-04 11:50:50 -0800

    add toggle check function and connect to localstorage

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -2,35 +2,37 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 
-import { deleteTask } from '../tasksSlice';
+import { deleteTask, toggleDone } from '../tasksSlice';
 
 const TasksList = () => {
   const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-  console.log(tasks);
 
   const onDeleteClicked = (event) => {
     const index = event.target.dataset.index;
-    console.log(index);
     dispatch(deleteTask({ index }));
+  };
+
+  const onCheckClicked = (event) => {
+    const index = event.target.dataset.index;
+    console.log(index);
+    dispatch(toggleDone({ index }));
   };
 
   const renderTasks = tasks.map((task, index) => {
     return (
       <li key={index}>
         <label htmlFor={`task-${index}`}>{task.name}</label>
-        <div>
-          <input
-            type='checkbox'
-            id={`task-${index}`}
-            data-index={index}
-            defaultChecked={task.isDone}
-            // onClick={onToggleDone}
-          />
-          <button data-index={index} onClick={onDeleteClicked}>
-            Delete
-          </button>
-        </div>
+        <input
+          type='checkbox'
+          id={`task-${index}`}
+          data-index={index}
+          defaultChecked={task.isDone}
+          onClick={onCheckClicked}
+        />
+        <button data-index={index} onClick={onDeleteClicked}>
+          Delete
+        </button>
       </li>
     );
   });
