921df0e35d8204553424ca61acb79baadca80aad
Pichborith Kong <pichborith@gmail.com>
2023-02-03 21:06:34 -0800

    update to use Redux

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
 
-const InputBox = ({ onInputChange, onSubmitAdd }) => {
+import { taskAdded } from '../tasksSlice';
+
+const InputBox = () => {
+  const [task, setTask] = useState('');
+  const dispatch = useDispatch();
+
+  const onInputChange = (event) => {
+    setTask(event.target.value);
+    console.log(task);
+  };
+
+  const onSubmitAdd = (event) => {
+    event.preventDefault();
+    if (task) {
+      dispatch(taskAdded({ id: nanoid(), name: task, isDone: false }));
+    }
+    setTask('');
+    event.target.reset();
+  };
+
   return (
-    <form className='input-box' onSubmit={(event) => onSubmitAdd(event)}>
+    <form className='input-box' onSubmit={onSubmitAdd}>
       <input
         type='text'
         name='task'
